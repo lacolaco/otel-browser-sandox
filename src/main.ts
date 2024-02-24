31adc0910a070ba8ad05b1ce66b8b687df826281
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-02-24 17:21:16 +0900

    fix: disable sentry auto instrumentation

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { bootstrapApplication } from "@angular/platform-browser";
-import { appConfig } from "./app/app.config";
-import { AppComponent } from "./app/app.component";
 import * as Sentry from "@sentry/angular-ivy";
+import { AppComponent } from "./app/app.component";
+import { appConfig } from "./app/app.config";
 
 Sentry.init({
   dsn: "https://a9da6c07af5a10b86a16b55f08c99339@o4505734726549504.ingest.sentry.io/4506800643440640",
-  integrations: [Sentry.browserTracingIntegration()],
+  // integrations: [Sentry.browserTracingIntegration()],
   // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost"],
+  // tracePropagationTargets: ["localhost"],
 });
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
