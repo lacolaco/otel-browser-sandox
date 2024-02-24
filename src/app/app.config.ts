a31b34dce0acc0dfe537cd60ed1283d55d66d550
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-02-24 16:51:32 +0900

    fix: install sentry sdk

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { APP_INITIALIZER, ApplicationConfig } from "@angular/core";
+import { Router, provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
+import { TraceService } from "@sentry/angular-ivy";
+
+function provideSentryTraceService() {
+  return [
+    {
+      provide: TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [TraceService],
+      multi: true,
+    },
+  ];
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideSentryTraceService()],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { AppComponent } from "./app/app.component";
+import * as Sentry from "@sentry/angular-ivy";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+Sentry.init({
+  dsn: "https://a9da6c07af5a10b86a16b55f08c99339@o4505734726549504.ingest.sentry.io/4506800643440640",
+  integrations: [Sentry.browserTracingIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+});
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
